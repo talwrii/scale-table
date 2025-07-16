e3050433e721f4eca5a42667b2592878780a5a9f
Tal Wrii <talwrii@gmail.com>
2025-07-16 16:11:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# scale table
+**@readwithai** - [X](https://x.com/readwithai) - [blog](https://readwithai.substack.com/) - [machine-aided reading](https://www.reddit.com/r/machineAidedReading/) - [📖](https://readwithai.substack.com/p/what-is-reading-broadly-defined
+)[⚡️](https://readwithai.substack.com/s/technical-miscellany)[🖋️](https://readwithai.substack.com/p/note-taking-with-obsidian-much-of)
+
+A table of various scales and their relationships and means to generate them.
+
+## Motivation
+I learned to play the guitar by learning intervals rather than scales. This probably have some benefits - notes are kind of arbitrary can can get in the way - but notes are interesting too, so I am learning these.
+
+With this in mind, I am going to improvish on the guitar while paying attention to scales on a chart. This is code to build this chart (because I am too lazy to do it manually in a spreadsheet).
+
+Of course, formalized learning through learning pieces and exercises probably has some benefits... but I'm still probably not going to do it.
+
+## The table
+<a href="scale.pdf"> <img src="scale.png" alt="scale"> </img> </a>
+
+
+## Usage
+You can copy this table and print it. If you want to regenerate you can:
+
+* clone this repo
+* Create a venv with `python -m venv env`
+* Install requirements iwth `env/bin/pip install -r requirements.txt`
+* Regenerates `scale.pdf` with `env/bin/python scale..py`
+
+## About me
+I am **@readwithai**. I create tools for reading, research and agency sometimes using the markdown editor [Obsidian](https://readwithai.substack.com/p/what-exactly-is-obsidian).
+
+I also create a [stream of tools](https://readwithai.substack.com/p/my-productivity-tools) that are related to carrying out my work.
+
+I write about lots of things - including tools like this - on [X](https://x.com/readwithai).
+My [blog](https://readwithai.substack.com/) is more about reading and research and agency.
---
+++ scale.py
@@ -0,0 +1,133 @@
+import collections
+from reportlab.lib.pagesizes import landscape, A4
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+
+
+MAJOR = (0, 2, 4, 5, 7, 9, 11)
+
+def major_scale(x):
+    return [(x + i) % 12 for i in MAJOR]
+
+def list_transpose(n, xs):
+    return tuple(transpose(n, x) for x in xs)
+
+def transpose(n, x):
+    return (x + n) % 12
+
+
+def fifth(x):
+    return transpose(7, x)
+
+def list_fifth(xs):
+    return list_transpose(7, xs)
+
+def flat_name(x):
+    return {
+        0: 'C',
+        1: 'Db',
+        2: 'D',
+        3: 'Eb',
+        4: 'E',
+        5: 'F',
+        6: 'Gb',
+        7: 'G',
+        8: 'Ab',
+        9: 'A',
+        10: 'Bb',
+        11: 'B'}[x]
+
+def sharp_name(x):
+    return {
+        0: 'C',
+        1: 'C#',
+        2: 'D',
+        3: 'D#',
+        4: 'E',
+        5: 'F',
+        6: 'F#',
+        7: 'G',
+        8: 'G#',
+        9: 'A',
+        10: 'A#',
+        11: 'B'
+    }[x]
+
+def sharp_name_list(xs):
+    return list(map(sharp_name, xs))
+
+def letter_collision(names):
+    return max(collections.Counter(n[0] for n in names if n != '').values()) != 1
+
+def draw_row(xs):
+    pitches = [i if i in xs else None for i in range(12)]
+    flats = [flat_name(p) if p is not None else '' for p in pitches]
+    sharps = [sharp_name(p) if p is not None else '' for p in pitches]
+    if not letter_collision(flats):
+        return flats
+    elif not letter_collision(sharps):
+        return sharps
+    else:
+        raise Exception(f'Both sharps and flats have collisions: {flats} {sharps}')
+
+def repeat(n, f, x):
+    r = x
+    for i in range(n):
+        r = f(r)
+    return r
+
+
+
+style = TableStyle()
+
+rows = []
+circle_of_fifths = [repeat(i, fifth, 0) for i in range(12)]
+for repeat in (0, 1):
+    for i, x in enumerate(circle_of_fifths):
+        row_num = i + 12 * repeat
+        del i
+
+        if row_num % 2 == 0:
+            style.add('BACKGROUND', (0, row_num), (-1, row_num), colors.gainsboro)
+
+        if x == 6:
+            row = ['', 'C#', '', 'D#', '',  'E#', 'F#', '', 'G#', '', 'A#', 'B']
+        else:
+            row = draw_row(major_scale(x))
+
+
+        minor = (x - 3) % 12
+
+        row = [c or '*' for c in row]
+
+        for k in (0, 1):
+            style.add('BACKGROUND', (x + 12 * k, row_num), (x + 12 * k, row_num), colors.lightgreen)
+            style.add('BACKGROUND', (minor + 12 * k, row_num), (minor + 12 * k, row_num), colors.violet)
+
+        double_row = row + row
+
+        for i, name in enumerate(double_row):
+            if '#' in name:
+                style.add('TEXTCOLOR', (i, row_num), (i, row_num), colors.red)
+
+            if 'b' in name:
+                style.add('TEXTCOLOR', (i, row_num), (i, row_num), colors.blue)
+
+        rows.append(double_row)
+
+
+for col in range(0, 24, 2):
+    style.add('BACKGROUND', (col, 0), (col, -1), colors.lightgrey)
+
+
+
+style.add('ALIGN', (0, 0), (-1, -1), 'CENTER')
+
+
+
+
+
+doc = SimpleDocTemplate("scale.pdf", pagesize=landscape(A4))
+table = Table(rows)
+table.setStyle(style)
+doc.build([table])
